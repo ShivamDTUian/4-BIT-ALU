2cb273f5ffc55be7692722bdee8966d8b7621eb1
ShivamDTUian <shivam109.m@gmail.com>
2024-09-06 23:24:20 +0530

    Update 4BitALU.v

--- 4BitALU.v
+++ 4BitALU.v
@@ -30,17 +30,17 @@ module ALU_4bit(Out, Z, C, V, P, A, B, Opcode);
 	Arithmetic a(arith_out, temp_C, temp_V, A, B, Opcode);
 	Comparator c(comp_out, A, B, Opcode);
 	Logical l(logical_out, A, B, Opcode);
-	
+
 	MUX m(Out, shift_out, arith_out, logical_out, comp_out, Opcode);
-	
+
 	assign C = (Opcode > 3 && Opcode < 8) ? temp_C
 				: 1'b0;
 
 	assign V = (Opcode > 3 && Opcode < 8) ? temp_V
 				: 1'b0;
-				
+
 	assign P = Out[3] ^ Out[2] ^ Out[1] ^ Out[0];
-	
+
 	assign Z = (Out[3] == 0) & (Out[2] == 0) & (Out[1] == 0) & (Out[0] == 0);
-	
+
 endmodule
